84f72fb3fd7e1e3111c502177e9a27aa8f3ab0a6
Masaki.Nakano <admin@namachan10777.dev>
2023-10-16 23:20:40 +0900

    job generator

---
+++ scripts/gen_tests.rb
@@ -0,0 +1,23 @@
+require 'erb'
+
+template = IO.read("template.sh.erb")
+server_thread_count_max = 256
+client_thread_count_max = 256
+client_actual_task_count_max = 1024
+nodes = 2
+
+template = ERB.new(template)
+
+server_thread_count = 1
+while server_thread_count <= server_thread_count_max
+  client_thread_count = 1
+  while client_thread_count <= client_thread_count_max
+    client_task_count = 1
+    while client_thread_count * client_task_count <= client_actual_task_count_max
+      IO.write("#{server_thread_count}-#{client_thread_count}-#{client_task_count}.sh", template.result)
+      client_task_count *= 2
+    end
+    client_thread_count *= 2
+  end
+  server_thread_count *= 2
+end
